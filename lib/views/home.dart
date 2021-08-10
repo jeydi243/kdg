e8ec6c5679045e02a462a8776090e495b966d4ae
jeydi <kadiongoe@yahoo.fr>
2021-08-10 16:22:42 +0200

    fix

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -32,7 +32,7 @@ class _HomeState extends State<Home> {
           ],
         ),
         body: SafeArea(
-          child: Section1(), 
+          child: Text("le monde est beau"), 
 
         ));
   }
